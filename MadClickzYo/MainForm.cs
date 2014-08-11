9e4ec706e652d36610da04941c8acb47c5f9cb4f
mrajo <anthony.c@stle.co>
2014-08-11 13:03:41 -0700

    Replacing external mouse_event function with InputSimulator library.

--- MadClickzYo/MainForm.cs
+++ MadClickzYo/MainForm.cs
@@ -11,19 +11,13 @@ using System.Timers;
 using System.Windows.Forms;
 using MouseKeyboardActivityMonitor;
 using MouseKeyboardActivityMonitor.WinApi;
+using WindowsInput;
 
 namespace MadClickzYo
 {
     public partial class MainForm : Form
     {
-        [DllImport("user32.dll")]
-        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
-
-        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        private const int MOUSEEVENTF_LEFTUP = 0x04;
-        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
-        private const int MOUSEEVENTF_RIGHTUP = 0x10;
-
+        private InputSimulator input;
         private readonly MouseHookListener mouseHook;
         private readonly KeyboardHookListener keyboardHook;
         private System.Timers.Timer timer;
@@ -39,6 +33,7 @@ namespace MadClickzYo
             keyboardHook.Enabled = true;
             keyboardHook.KeyPress += KeyboardEvent_KeyPress;
 
+            input = new InputSimulator();
         }
 
         private void KeyboardEvent_KeyPress(object sender, KeyPressEventArgs e)
@@ -69,8 +64,10 @@ namespace MadClickzYo
 
         private void AutoClickEvent(Object source, ElapsedEventArgs e)
         {
+            // getting the InputSimulator to move the mouse to an absolute position on
+            // multiple monitors in staggered positions is a pain, so just use Cursor for now
             Cursor.Position = new Point(Int32.Parse(txtClickX.Text), Int32.Parse(txtClickY.Text));
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, UIntPtr.Zero);
+            input.Mouse.LeftButtonClick();
         }
     }
 }
